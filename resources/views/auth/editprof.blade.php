cad51358dd79aaa941a21af726c4161a217d9089
user <user@usernoMacBook-puro-2.local>
2020-01-25 13:21:08 +0900

    編集画面フォーム作成

--- resources/views/auth/editprof.blade.php
+++ resources/views/auth/editprof.blade.php
@@ -13,6 +13,34 @@
 <h1>編集画面</h1>
 
 
+@if (count($errors) > 0)
+<div>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<table>
+<form action="'/home/profiel/'.Auth::id()" method="post">
+    {{ csrf_field() }}
+    <input type="hidden" name="id" value="{{Auth::id()}}">
+    <tr><th>Name:</th><td><input type="test" name="name" value="{{$form->name ?? '' }}"></td></tr>
+    <tr><th>birthday:</th><td><input type="date" name="name" value="{{$form->birthday ?? '' }}"></td></tr>
+    <tr><th>job:</th><td><input type="test" name="name" value="{{$form->job ?? '' }}"></td></tr>
+    <tr><th>skill:</th><td><input type="test" name="name" value="{{$form->skill ?? '' }}"></td></tr>
+    <tr><th>hobby:</th><td><input type="test" name="name" value="{{$form->hobby ?? '' }}"></td></tr>
+    <tr><th>email:</th><td><input type="email" name="name" value="{{$form->email ?? '' }}"></td></tr>
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
+</form>
+</table>
+
+
+
+
+
 @auth
 <p>idは{{$id}}です</p>
 <br>
--- resources/views/auth/profiel.blade.php
+++ resources/views/auth/profiel.blade.php
@@ -14,17 +14,25 @@
 
 
 <table>
-{{ Auth::id() }}
+
+<tr><th>ID</th><td>{{$user -> id}}</td></tr>
+<tr><th>Name</th><td>{{$user -> name}}</td></tr>
+<tr><th>birthday</th><td>{{$user -> birthday}}</td></tr>
+<tr><th>job</th><td>{{$user -> job}}</td></tr>
+<tr><th>skill</th><td>{{$user -> skill}}</td></tr>
+<tr><th>hobby</th><td>{{$user -> hobby}}</td></tr>
+<tr><th>email</th><td>{{$user -> email}}</td></tr>
+<tr><th>updated_at</th><td>{{$user -> updated_at }}</td></tr>
+</table>
+
+
 <br>
+
 {{$user }}
-<br>
-{{$user -> name}}
-<br>
-{{$name}}
+
 <br>
 
 @auth
-<p>idは{{$id}}です</p>
 <br>
 <a href="{{ url('/home/editprof/'.Auth::id()) }}">プロフィールを編集する</a>
 <br>
@@ -34,7 +42,7 @@
 
 
 
-</table>
+
 
 
 
