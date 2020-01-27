30b5e068e31eb1a09e41f4f6ea031d2b16f6a94d
user <user@usernoMacBook-puro-2.local>
2020-01-27 21:39:59 +0900

    messagesテーブルとrelationshipsテーブルの作成

---
+++ database/migrations/2020_01_27_122857_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('send_user_id');
+            $table->integer('receive_user_id');
+            $table->text('message_text');
+            $table->boolean('is_readed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
---
+++ resources/views/profiel/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<h1>編集画面</h1>
+
+
+@if (count($errors) > 0)
+<div>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+
+<form method="post" action="/profiel/update/{{Auth::id()}}">
+
+<table>
+{{ csrf_field() }}
+    <input type="hidden" name="id" value="{{Auth::id()}}">
+    
+    <tr><th>Name:</th><td>
+    <input type="text" name="name" value="{{$form->name ?? '' }}"></td></tr>
+
+    <tr><th>birthday:</th><td><input type="date" name="birthday" value="{{$form->birthday ?? '' }}"></td></tr>
+
+    <tr><th>job:</th><td><input type="text" name="job" value="{{$form->job ?? '' }}"></td></tr>
+
+    <tr><th>skill:</th><td><input type="text" name="skill" value="{{$form->skill ?? '' }}"></td></tr>
+
+    <tr><th>hobby:</th><td><input type="text" name="hobby" value="{{$form->hobby ?? '' }}"></td></tr>
+
+    <tr><th>email:</th><td><input type="email" name="email" value="{{$form->email ?? '' }}"></td></tr>
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
+    </table>
+</form>
+
+
+
+
+
+
+
+
+@auth
+<a href="{{ url('/profiel/index/'.Auth::id()) }}">プロフィール画面へ</a>
+<br>
+@endauth
+
+
+
+
+@endsection
