d26a2e3b3199ead72d8aa1d523c9996dc969b9da
user <user@usernoMacBook-puro-2.local>
2020-01-30 22:57:17 +0900

    チャット画面へ遷移

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class ChatController extends Controller
+{
+
+    public function index(Request $request){
+
+        $user = Auth::id();
+        $friend = User::find($request->id);
+
+        
+        return view('friend',['friend' => $friend]);
+        
+    }
+    
+    public function send(Request $request){
+
+        $user = Auth::User();
+        $friend_id = $request->id;
+
+
+        return view('friend/',['friend_id' => $friend_id]);
+
+
+    }
+}
---
+++ resources/views/friend.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<h1>チャット画面</h1>
+
+<br>
+
+<h1><th>Name</th><td>{{$friend->name}}</td></h1>
+
+
+@endsection
+
+
