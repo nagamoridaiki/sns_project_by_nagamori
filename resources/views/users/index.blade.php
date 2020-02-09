7fcd401e58a7d3046447e00913da98f422012c4d
user <user@usernoMacBook-puro-2.local>
2020-02-09 17:42:56 +0900

    レイアウトをグリーン系に変えてみた

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,76 +4,97 @@
 
 
 <div id="profiel">
-    <h1>プロフィール画面</h1>
-    <table>
-
-    <tr><th>ID</th><td>{{$user -> id}}</td></tr>
-    <tr><th>Name</th><td>{{$user -> name}}</td></tr>
-    <tr><th>birthday</th><td>{{$user -> birthday}}</td></tr>
-    <tr><th>job</th><td>{{$user -> job}}</td></tr>
-    <tr><th>skill</th><td>{{$user -> skill}}</td></tr>
-    <tr><th>hobby</th><td>{{$user -> hobby}}</td></tr>
-    <tr><th>email</th><td>{{$user -> email}}</td></tr>
-    <tr><th>updated_at</th><td>{{$user -> updated_at }}</td></tr>
-    </table>
-
-    @auth
-    <br>
-    <a href="{{ url('/users/edit/'.Auth::id()) }}">プロフィールを編集する</a>
-    <br>
-    @endauth
+    <div class="profiel-line">
+        <div class="profiel-head">
+            <h3>プロフィール画面</h3>
+        </div>
+        <div class="profiel-table">
+            <table>
+            <tr><th>ID</th><td>{{$user -> id}}</td></tr>
+            <tr><th>Name</th><td>{{$user -> name}}</td></tr>
+            <tr><th>birthday</th><td>{{$user -> birthday}}</td></tr>
+            <tr><th>job</th><td>{{$user -> job}}</td></tr>
+            <tr><th>skill</th><td>{{$user -> skill}}</td></tr>
+            <tr><th>hobby</th><td>{{$user -> hobby}}</td></tr>
+            <tr><th>email</th><td>{{$user -> email}}</td></tr>
+            <tr><th>updated_at</th><td>{{$user -> updated_at }}</td></tr>
+            </table>
+        </div>
+        <div class="profiel-edit">
+            @auth
+            <br>
+            <a href="{{ url('/users/edit/'.Auth::id()) }}">プロフィールを編集する</a>
+            <br>
+            @endauth
+        </div>
+    </div>
 </div>
 
 
 
-<div id="friend_list">
-    <tr>
-    <table class="table">
-        <thead>
-            <td><h2>友達</h1></td>
-            <td>
-                <form method="post" action="/users/show/{{Auth::id()}}">
-                    {{ csrf_field() }}
+<div class="friend_top">
+    <div class="friend-main">
+        <div class="friend-theme">
+            <h3>友達</h3>
+        </div>
+        <div class="friend-serch">
+            名前で検索
+            <form class="form" method="post" action="/users/show/{{Auth::id()}}">
+                {{ csrf_field() }}
+                <div class="serch-window">
                     <input type="text" name="name">
+                </div> 
+                <div class="serch-submit">
                     <input type="submit" value="検索">
-                </form>
-            </td>
-        </thead>
-        <tr>
-            <td>#</td>
-            <td>Name</td>
-        </tr>
-        
-        <tbody>
-        @foreach($others as $key => $user)
-        
-            <tr>
-                <th>{{$loop->iteration}}</th>
-                <td>{{$user->name}}</td>
+                </div>      
+            </form>
+        </div>
+        <div class="friend-table">
+            <table>
+                <div class="friend-table-body">
+                    <tbody>
+                    @foreach($others as $key => $user)
+                    
+                        <tr>
+                            <th>
+                                <div class="iteration">
+                                    {{$loop->iteration}}
+                                </div>  
+                            </th>
+                            <td>
+                                <div class="itr-name">
+                                    {{$user->name}}
+                                </div>
+                            </td>
+                            <td>
+                                <a href="{{ url('/messages/index/'.$user->id) }}">
+                                    <button type="button" class="btn btn-primary">chat</button>
+                                </a>
+                            </td>
 
-                <td><a href="{{ url('/messages/index/'.$user->id) }}"><button type="button" class="btn btn-primary">chat</button></a></td>
+                            @php
+                                $flag = 0;
+                                foreach($relationships as $index => $value){
+                                    if($value->friend_id == $user->id){
+                                        //echo 'すでに友達'.'<br>';
+                                        $flag = 1;
+                                    }
+                                }
+                            @endphp
 
-                @php
-                    $flag = 0;
-                    foreach($relationships as $index => $value){
-                        if($value->friend_id == $user->id){
-                            //echo 'すでに友達'.'<br>';
-                            $flag = 1;
-                        }
-                    }
-                @endphp
-
-                @if($flag == 0)
-                    <td><a href="{{ url('/users/create/'.$user->id) }}"><button type="button" class="btn btn-primary">フォローする</button></a></td>
-                @else
-                    <td>友達になっています　<a href="{{ url('/users/destroy/'.$user->id) }}"><button type="button" class="btn btn-primary">フォローを外す</button></a></td>
-                @endif
-            </tr>
-        @endforeach
-        </tbody>
-
-    </table>
+                            @if($flag == 0)
+                                <td><a href="{{ url('/users/create/'.$user->id) }}"><button type="button" class="btn btn-primary">フォローする</button></a></td>
+                            @else
+                                <td><a href="{{ url('/users/destroy/'.$user->id) }}"><button type="button" class="btn btn-primary">フォローを外す</button></a></td>
+                            @endif
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </div>
+            </table>
+        </div>
     </div>
+</div>
 
 @endsection
 
